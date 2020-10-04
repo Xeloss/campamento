7794e0616ee76b337052f09dbce9c245c448b3a9
Galu <galluppi.leandro@hotmail.com>
2020-10-04 13:07:53 -0300

    - Init

---
+++ campamento.js
@@ -0,0 +1,193 @@
+/* MASTIL   MASTIL   MASTIL   MASTIL   MASTIL   MASTIL   */
+contenidoMastil ='<h2>Mastil</h2>\
+<p>¡Aquí nos reunimos para hacer ejercicio! 🏋️‍♀️🤸🏋️‍♀️</p>\
+<p>Reune a tu familia y dadle al play... ¿Podréis seguirnos el ritmo?</p>\
+<h6>Lunes: <a href="https://youtu.be/CT_tYFHPxq0" target="_blank"> CLICK AQUÍ - CALENTAMOS MOTORES!</a></h6>\
+<h6>Martes: <a href="https://youtu.be/EbGWHq-fwOw" target="_blank"> CLICK AQUÍ - MANTENEMOS EL RITMO!</a></h6>\
+<h6>Miércoles: <a href="https://youtu.be/G0Tc8PUYoLU" target="_blank"> CLICK AQUÍ - TRAMO FINAL!</a></h6>\
+<p><b>No olvidéis estirar después de hacer los ejercicios 🧘</b></p>\
+';
+
+
+/* ERA  ERA  ERA  ERA  ERA  ERA  ERA   */
+contenidoEra ='<h2>La Era 🔥😍</h2>\
+<p>Aquí tendremos cada día una actividad CONJUNTA.</p>\
+<p><b>¡SÍ, CONJUNTA!</b> ¡Ven a la hora, puntual, no te retrases!</p>\
+<h6>Lunes 18:00 : Día de la Vª: Videoconferencia zoom! (Consultad vuestros whatsapps a las 16:00!)</h6>\
+<h6>Martes: 18:00:  Party y Pictionary Scout! Preparad el zoom, la pizarra/papel y rotu!</h6>\
+<h6>Miércoles: 19:00: ¡Velada CON DISFRACES!</h6>\
+';
+
+
+/* BORDA    BORDA    BORDA    BORDA    BORDA    */
+contenidoBorda ='<h2>Cocina Borda 🥘🍽️🍝</h2>\
+<p>Todo el mundo sabe que los mejores manjares son los que se sirven en la borda..</p>\
+<p>Estos tres días, cocina para tu familia (puedes pedirles que te ayuden). Únete al concurso <b>Master KintaChef</b>, enviános tus creaciones y <b>gana premios!</b></p>\
+<h6>Lunes: <a href="https://youtu.be/YhDKsblgvA0" target="_blank"> CLICK AQUÍ - Pizza!</a></h6>\
+<h6>Martes: <a href="https://youtu.be/2Z2Qv6RYh6c" target="_blank">CLICK AQUÍ - Tarta de queso!</a> </h6>\
+<p> Y <a href="https://youtu.be/JNP_KWe8A0I" target="_blank">la familia Arrondo Ruiz nos envía esta pedazo receta de fajitas</a>! </p>\
+<h6>Miércoles: <a href="https://youtu.be/ol-5c44WFKA" target="_blank"> CLICK AQUÍ - Lasagna!</a></h6>\
+<p>¿Se te ocurren recetas mejores? Mira qué queda en la nevera, ponte manos a la masa y enviános videos explicando como se cocina! Consulta las <a href="https://drive.google.com/open?id=1ibkeaSKRXvJNRbKo3NO-b9BBDu7kvDp5" target="_blank">recetas tradicionales de la kinta!</p>\
+';
+
+/*  TXOKO    TXOKO    TXOKO    TXOKO     */
+contenidoTxoko ='<iframe width="100%" height="120" src="https://www.youtube.com/embed/Qm846KdZN_c?autoplay=1&controls=0"> </iframe>\
+<h2>Txoko del Silencio</h2>\
+<p>Un lugar de calma y paz, donde observar y reflexionar al ritmo que nos pide el cuerpo, no el que nos impone la vida.</p>\
+<p>Ponte auriculares, escucha a la naturaleza y, cuando te sientas preparad@, abre el baul con materiales que hemos dejado junto a las raices de esta fabulosa haya.</p>\
+<p>Antes de abrir el baul, <a href="https://drive.google.com/file/d/1YU6TXvAsW60HurrSbH2XqZvBiROhJQKg/view?usp=drivesdk" target="_blank">acude a la asamblea en la carpinteria</a>.</p>\
+<a href="https://drive.google.com/open?id=16PKtBEOU81jq3z3eYmlu2z_xNqyGGeyp" target="_blank"><button type="button" class="btn btn-success" > <img src="./img/baul.png" height=20/> Abril baul</button></a>\
+';
+
+/* VIDEOCLIP   VIDEOCLIP   VIDEOCLIP   VIDEOCLIP   VIDEOCLIP    */
+contenidoVideoclips ='<h2>📹📼 </h2>\
+<p>¡Ya hemos completado dos terceras partes del video! Pero necesitamos mas ayuda!</b></p>\
+<p>🐯 Shere Khan quiere que te aburras en casa, quiere que te preocupes y que lo pases mal. Cree que por estar en casa este campamento lo vas a pasar fatal...</p>\
+<p>🐺 Akela nos ha pedido que le ayudemos para demostrarle que se equivoca! Para ello tenéis que conseguir grabar y enviarnos suficientes videos de "<a href="https://www.youtube.com/watch?v=ttlecah_IFs" target="_blank">busca lo mas vital</a>" para conseguir rellenar el videoclip sin que salga ningún/a monitor/a </p>\
+<a href="https://www.youtube.com/watch?v=Xto2u_kItfE" target="_blank"><img src="./img/play.jpg" width=350/><a/>\
+<p>Envia tu trozo de video por whatsapp, <a href="https://www.instagram.com/vtropascouts/" target="_blank">instagram</a>, <a href="https://www.facebook.com/scoutvtropa/" target="_blank">facebook</a> o a <a href="mailto:web.scoutvtropa@gmail.com" target="_blank">web.scoutvtropa@gmail.com</a></p>\
+';
+
+/* TALLERES   TALLERES   TALLERES   TALLERES   TALLERES    */
+contenidoTalleres ='<h2>Talleres ✂️🖍️🧵</h2>\
+<p>Te proponemos un taller por día para que demuestres tus habilidades. Enviános fotos del proceso o del resultado y <b>puedes ganarte un premio eskaut!</b></p>\
+<h6>Lunes: <a href="https://youtu.be/E_lUQ7WIz0o" target="_blank"> CLICK AQUÍ - Bolsas de tela</a> y <a href="https://youtu.be/L95WowsjPhU" target="_blank"> Retrato express!</a></h6>\
+<h6>Martes: <a href="https://youtu.be/EZl3knlMTFo" target="_blank">CLICK AQUÍ - Cabuyería (nudos) nivel principiante</a> y <a href="https://youtu.be/udoWH5xybFI" target="_blank">nivel avanzado</a></h6>\
+Y reto express de hoy: Disfrazarnos de nuestro tótem/animal favorito. Anímate y anima a tu familia a hacerlo! </p>\
+<h6>Miércoles: <a href="https://youtu.be/-HqpQEmNEOk" target="_blank">CLICK AQUÍ - Estampado de camisetas!</a> y <a href="https://youtu.be/UTS7PV6QPNg" target="_blank"> Reto express DIBUJO COMPARTIDO!</a></h6>\
+';
+
+
+/* MUSEO   MUSEO   MUSEO   MUSEO     */ 
+contenidoMuseo ='<h2>Museo de antigüedades</h2>\
+<p>¿Eres mayor de edad? Me temo que eso significa que eres antigu@. ¡O vieja gloria si lo prefieres!</p>\
+<p>Vente al museo a echar un vistazo a fotos de otros tiempos. Búscate y encuentra a tus viej@s colegas.</p>\
+<p>Oferta: Entrada gratuita presentando tu pañoleta de la Vª</p>\
+<a href="https://photos.app.goo.gl/MJfjTPTBJin6HMDH9" target="_blank"><button type="button" class="btn btn-success" >Entrar al museo</button></a>\
+<p>¡<a href="mailto:web.scoutvtropa@gmail.com" target="_blank">Enviános tus fotos de antaño</a> para que las añadamos al múseo! <span class="smallText">(o escribenos si quieres retirar alguna)</span></p>\
+';
+
+/* LOBATOS    LOBATOS    LOBATOS    LOBATOS LOBATOS  */
+contenidoLobis ='<h2>Campa de lobat@s</h2>\
+<h6>Lunes: Máxima -> El lobato y la lobata es alegre y sonriente.</h6>\
+<p><a href="https://photos.app.goo.gl/zbkA5k7RREVD5sed9" target="_blank">Album original</a>. <a href="https://photos.google.com/share/AF1QipNEOWo-4hZbZpwHK_SfHaSfskyjG2s3N8VOQdjBntbTWmB0vNEN6dNUCsfYFRZPQQ?key=b0ZPWlRBaExoQjRfVlJrU2cwbVo1cEx2ajY5M1JR" target="_blank">Aquí vuestras contribuciones!</a>\
+<h6>Martes: Máxima -> El lobato y la lobata piensa antes en los demás.</h6>\
+<p><a href="https://drive.google.com/open?id=1S5oeoKqcOPqzXOPTlhYc-ZBPMFHSJOeV" target="_blank">Reto del “yo nunca” </a></p>\
+</p>\
+<h6>Miércoles: Máxima -> El lobato y la lobata tiene los ojos y oídos bien abiertos</h6>\
+<p>El reto de hoy es que encontréis <a href="https://drive.google.com/drive/folders/1Mt7tkLxZVTsKAF0okq8_9sIIa9KBvq32" target="_blank">en cada foto donde está la pañoleta escondida</a>! Mandarnos comentarios cuando la hayáis encontrado!</p>\
+';
+
+
+
+/* RAMAS    RAMAS    RAMAS    RAMAS    RAMAS     */
+contenidoRamas ='<h2>Bosque de las Ramas</h2>\
+<h5>Pre-rangers</h5>\
+<p>Nos ha llegado un carta de gandalf! <a href="./img/prerangers_carta.jpg" target="_blank">🧙 Click aquí para verla</a>.  Y <a href="https://youtu.be/J3BACY9qC60" target="_blank"> aquí el video de vuestr@s monis!</a></p/>\
+<h5>Rangers</h5>\
+<p><button class="btn btn-info btn-sm" onclick="$(\'#modal_leyes\').modal(\'show\');">Acceso a campa Rangers </button></p/>\
+<h5>Rutas</h5>\
+<p><button class="btn btn-info btn-sm" onclick="$(\'#modal_rutas\').modal(\'show\');">Acceso a campa Rutas </button></p/>\
+';
+
+
+
+/* ALBOAN    ALBOAN    ALBOAN    ALBOAN     */
+contenidoAlboan ='<h2>Claro Alboan</h2>\
+<p>Para visibilizar la unión de las personas de los 5 continentes queremos trenzar una cadena que recorra el mundo con los mensajes #SomosCiudadaníaGlobal y #YoMeQuedoEnCasa.</p>\
+<p>Un símbolo de apoyo mutuo y unidad global en tiempos de coronavirus.</p>\
+<h5><a href="" target="_blank">Te lo explicamos en este video!</a><h5>\
+<iframe width="100%" height="120" src="https://www.youtube.com/embed/-Mi1Gl-lUqw?autoplay=1&controls=0"> </iframe>\
+<p> <a href="https://www.alboan.org/es/actualidad/internacional/trenza-una-cadena-de-solidaridad-somosciudadaniaglobal" target="_blank">Y + info en la web de ALBOAN.</a></p>';
+
+
+
+
+
+
+jQuery(document).ready(function($) {
+    $('#modal_bienvenida').modal('show');
+    //mapa base
+    var map = L.map('map', {
+        crs: L.CRS.Simple,
+        minZoom: -5,
+        attributionControl: false
+    });
+
+    var bounds = [[0,0], [600,800]];
+    var maxBounds = [[-700,-900], [1300,1300]];
+    var image = L.imageOverlay('./images/map.jpg', bounds).addTo(map);
+    map.fitBounds(bounds);
+    map.setMaxBounds(maxBounds);
+
+
+    //marcadores
+    // helper para transformar latitudes a coordenadas XY
+    var xy = function(x, y) {
+        if (L.Util.isArray(x)) {    // When doing xy([x, y]);
+            return yx(x[1], x[0]);
+        }
+        return yx(y, x);  // When doing xy(x, y);
+    };
+    var yx = L.latLng;
+    var mastil      = xy(175, 445);
+    var era    = xy( 430, 435);
+    var borda = xy( 520, 475);
+    var txoko    = xy( 725, 470);
+    var videoclips    = xy( 725, 190);
+    var talleres    = xy( 150, 65);
+    var museo    = xy( 325, 325);
+    var lobis    = xy( 470, 160);
+    var ramas = xy( 550, 315);
+    var alboan= xy( 630, 395);
+
+    L.marker(mastil).addTo(map).bindPopup(contenidoMastil);
+    L.marker(era).addTo(map).bindPopup(contenidoEra);
+    L.marker(borda).addTo(map).bindPopup(contenidoBorda);
+    L.marker(txoko).addTo(map).bindPopup(contenidoTxoko);
+    L.marker(videoclips).addTo(map).bindPopup(contenidoVideoclips,{maxWidth: "auto"});
+    L.marker(talleres).addTo(map).bindPopup(contenidoTalleres);
+    L.marker(museo).addTo(map).bindPopup(contenidoMuseo);
+    L.marker(lobis).addTo(map).bindPopup(contenidoLobis);
+    L.marker(ramas).addTo(map).bindPopup(contenidoRamas);
+    L.marker(alboan).addTo(map).bindPopup(contenidoAlboan);
+
+    //rrss
+    function onInstaClick(e) {
+        window.open("https://www.instagram.com/vtropascouts");
+    }
+    function onFBClick(e) {
+        window.open("https://www.facebook.com/scoutvtropa/");
+    }
+    var faceCords = [xy(5, 160), xy(120, 220)];   
+    var rectangle = L.rectangle(faceCords,{color: "rgba(47, 80, 242, 0.2)", weight: 1, border: "0px"}).addTo(map).on('click', onFBClick);
+    var instaCords =  [xy(5, 225), xy(120, 280)];       
+    var rectangle = L.rectangle(instaCords,{color: "rgba(222, 82, 22, 0.2)", weight: 1, border: "0px"}).addTo(map).on('click', onInstaClick);
+
+    //animales de la selva
+    function onAnimalClick(animal) {
+        $("#animal").html(animal);
+        $("#animal_img").attr("src","./img/"+animal+".png");
+        $('#modal_Animal').modal('show');
+    }
+    var akela = [xy(58, 353), xy(65, 360)];   
+    var rectangle = L.rectangle(akela,{color: "rgba(47, 80, 242, 0)", weight: 1, border: "0px"}).addTo(map).on('click', function(){ onAnimalClick("Akela")});
+    var kaa = [xy(690, 43), xy(698, 52)];   
+    var rectangle = L.rectangle(kaa,{color: "rgba(47, 80, 242, 0)", weight: 1, border: "0px"}).addTo(map).on('click', function(){ onAnimalClick("Kaa")});
+    var tigre = [xy(523, 228), xy(534, 239)];   
+    var rectangle = L.rectangle(tigre,{color: "rgba(47, 80, 242, 0)", weight: 1, border: "0px"}).addTo(map).on('click', function(){ onAnimalClick("Sherekhan")});
+    var bagueera = [xy(568, 491), xy(574, 503)];   
+    var rectangle = L.rectangle(bagueera,{color: "rgba(47, 80, 242, 0)", weight: 1, border: "0px"}).addTo(map).on('click', function(){ onAnimalClick("Bagheera")});
+    var mono = [xy(35, 131), xy(50, 145)];   
+    var rectangle = L.rectangle(mono,{color: "rgba(47, 80, 242, 0)", weight: 1, border: "0px"}).addTo(map).on('click', function(){ onAnimalClick("un mono")});
+    var hathi = [xy(648, 417), xy(655, 428)];   
+    var rectangle = L.rectangle(hathi,{color: "rgba(47, 80, 242, 0)", weight: 1, border: "0px"}).addTo(map).on('click', function(){ onAnimalClick("Hathi")});
+    var buitres = [xy(729, 510), xy(738, 520)];   
+    var rectangle = L.rectangle(buitres,{color: "rgba(47, 80, 242, 0)", weight: 1, border: "0px"}).addTo(map).on('click', function(){ onAnimalClick("los buitres")});
+    var baloo = [xy(557, 104), xy(571, 113)];    
+    var rectangle = L.rectangle(baloo,{color: "rgba(47, 80, 242, 0)", weight: 1, border: "0px"}).addTo(map).on('click', function(){ onAnimalClick("Baloo")});
+
+});
+
+
+
